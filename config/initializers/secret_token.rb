12bd8d3d7347e4789e23d6cef550e6f21ccf8a0e
HappyCup <1274474220qq.com>
2013-03-31 11:22:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsProjects::Application.config.secret_token = '476d17a119855f4c0169d01d93069bce519bcd6fcb76bf4b873a710f1d58e94303a9f49945e8303e36e1d8d51a44ac27f31366ac6b7ec34101d28312ab460eee'
